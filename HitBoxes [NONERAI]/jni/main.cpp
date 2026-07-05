52d8b22fc6ab3f598e58f02a192ba6585c50f247
Metadon-svg <krisevicu@gmail.com>
2026-07-05 15:27:11 +0300

    Update main.cpp

--- HitBoxes [NONERAI]/jni/main.cpp
+++ HitBoxes [NONERAI]/jni/main.cpp
@@ -33,7 +33,7 @@ using namespace std;
 void *main_thread(void *) {
     do { sleep(1); } while (!isLibraryLoaded(libName));
 
-    float MultiplyValue = 4;
+    float MultiplyValue = 2;
 	Utils::WriteMemory<float>(getAbsoluteAddress(libName, HEAD), 0.15f * MultiplyValue);
 	Utils::WriteMemory<float>(getAbsoluteAddress(libName, TORSO_1), 0.2f * MultiplyValue);
 	Utils::WriteMemory<float>(getAbsoluteAddress(libName, TORSO_2), 0.25f * MultiplyValue);
