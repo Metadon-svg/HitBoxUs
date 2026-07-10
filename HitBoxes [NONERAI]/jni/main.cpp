5d395e723ebf0699ca662b66d29efbb3784ef25c
Metadon-svg <krisevicu@gmail.com>
2026-07-10 17:58:55 +0300

    Update main.cpp

--- HitBoxes [NONERAI]/jni/main.cpp
+++ HitBoxes [NONERAI]/jni/main.cpp
@@ -1,91 +1,64 @@
 #include <iostream>
 #include <cstdio>
-#include <unistd.h> // Для usleep
+#include <unistd.h>
+#include <pthread.h>
 
-using namespace std;
-
+// Подключаем хукер (например, Dobby)
+#include "dobby.h" 
 #include "Utils.h"
 
-// HIT BOXES (find string - PedModelInfo.cpp line: 85)
-#if defined(__aarch64__)
-    uintptr_t HEAD = 0x14247F8 ;
-    uintptr_t TORSO_1 = HEAD + 0x20;
-    uintptr_t TORSO_2 = TORSO_1 + 0x20;
-    uintptr_t MID = TORSO_2 + 0x20;
-    uintptr_t LEFTARM = MID + 0x20;
-    uintptr_t RIGHTARM = LEFTARM + 0x20;
-    uintptr_t LEFTLEG_1 = RIGHTARM + 0x20;
-    uintptr_t RIGHTLEG_1 = LEFTLEG_1 + 0x20;
-    uintptr_t LEFTLEG_2 = RIGHTLEG_1 + 0x20;
-    uintptr_t RIGHTLEG_2 = LEFTLEG_2 + 0x20;
-#else
-    uintptr_t HEAD = 0xD31C88;
-    uintptr_t TORSO_1 = HEAD + 0x18;
-    uintptr_t TORSO_2 = TORSO_1 + 0x18;
-    uintptr_t MID = TORSO_2 + 0x18;
-    uintptr_t LEFTARM = MID + 0x18;
-    uintptr_t RIGHTARM = LEFTARM + 0x18;
-    uintptr_t LEFTLEG_1 = RIGHTARM + 0x18;
-    uintptr_t RIGHTLEG_1 = LEFTLEG_1 + 0x18;
-    uintptr_t LEFTLEG_2 = RIGHTLEG_1 + 0x18;
-    uintptr_t RIGHTLEG_2 = LEFTLEG_2 + 0x18;
-#endif
-
 #define libName "libblackrussia-client.so"
-
-// Путь во внутреннюю папку игры для стабильного чтения
 const char* filePath = "/storage/emulated/0/Android/data/com.br.top/files/Hitbox_Size.txt";
 
-float GetHitboxSize() {
-    float size = 1.0f; 
-    
-    FILE* file = fopen(filePath, "r");
-    if (file) {
-        fscanf(file, "%f", &size);
-        fclose(file);
+// Переменная, где ВСЕГДА лежит актуальный размер из файла
+float globalHitboxSize = 1.0f;
+
+// Смещение функции обновления кастомных калибров/костей педа (примерное, зависит от версии)
+// Тебе нужно будет найти точный оффсет функции типа SetupBones или СPed::Update в IDA/Gidra
+uintptr_t fnUpdateScaleOffset = 0x3A4B5C; 
+
+// Указатель на оригинальную функцию игры
+void (*orig_UpdateScale)(void* instance, float x, float y, float z);
+
+// Наша кастомная функция, которая подменяет размеры
+void hook_UpdateScale(void* instance, float x, float y, float z) {
+    // Вместо оригинальных x, y, z подставляем умноженные на наш globalHitboxSize
+    float newX = x * globalHitboxSize;
+    float newY = y * globalHitboxSize;
+    float newZ = z * globalHitboxSize;
+
+    // Вызываем оригинальную функцию уже с нашими «раздутыми» размерами
+    orig_UpdateScale(instance, newX, newY, newZ);
+}
+
+// Поток, который просто спокойно читает файл раз в полсекунды
+void *file_reader_thread(void *) {
+    while (true) {
+        float size = 1.0f;
+        FILE* file = fopen(filePath, "r");
+        if (file) {
+            fscanf(file, "%f", &size);
+            fclose(file);
+        }
+        // Обновляем глобальную переменную — хук сразу подхватит её в следующем кадре
+        globalHitboxSize = size; 
+        
+        usleep(500000); // 0.5 сек задержка
     }
-    return size;
+    return nullptr;
 }
 
 void *main_thread(void *) {
-    // Ждем загрузки либки игры
     do { sleep(1); } while (!isLibraryLoaded(libName));
 
-    // Получаем базовый адрес ОДИН раз, чтобы не грузить проц в цикле
     uintptr_t libBase = getAbsoluteAddress(libName, 0);
-    
-    float lastValue = -1.0f; 
-    int frameCounter = 0;
 
-    while (true) {
-        // Проверяем файл строго каждые 8 кадров
-        if (frameCounter >= 8) {
-            float MultiplyValue = GetHitboxSize();
+    // Ставим хук на функцию отрисовки/обновления масштаба костей
+    DobbyHook((void*)(libBase + fnUpdateScaleOffset), (void*)hook_UpdateScale, (void**)&orig_UpdateScale);
 
-            // Если значение в файле изменилось — обновляем адреса игры
-            if (MultiplyValue != lastValue) {
-                Utils::WriteMemory<float>(libBase + HEAD, 0.15f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + TORSO_1, 0.2f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + TORSO_2, 0.25f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + MID, 0.25f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + LEFTARM, 0.25f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + RIGHTARM, 0.16f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + LEFTLEG_1, 0.15f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + RIGHTLEG_1, 0.15f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + LEFTLEG_2, 0.15f * MultiplyValue);
-                Utils::WriteMemory<float>(libBase + RIGHTLEG_2, 0.15f * MultiplyValue);
-                
-                lastValue = MultiplyValue; 
-            }
-            
-            frameCounter = 0; // Сбрасываем счетчик
-        }
-
-        frameCounter++;
-
-        // Задержка ~16.6 миллисекунд (симуляция 60 кадров в секунду)
-        usleep(16666); 
-    }
+    // Запускаем фоновый поток для чтения текстового файла
+    pthread_t threadId;
+    pthread_create(&threadId, NULL, file_reader_thread, NULL);
 
     return nullptr;
 }
