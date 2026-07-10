ed30186de3e9b839da31647cb2b0d944c6c8abf3
Metadon-svg <krisevicu@gmail.com>
2026-07-10 15:53:48 +0300

    Update main.cpp

--- HitBoxes [NONERAI]/jni/main.cpp
+++ HitBoxes [NONERAI]/jni/main.cpp
@@ -4,36 +4,22 @@
 #include <jni.h>
 #include "Utils.h"
 
-// HIT BOXES 
-#if defined(__aarch64__)
-    uintptr_t HEAD = 0x14247F8 ;
-    uintptr_t TORSO_1 = HEAD + 0x20;
-    uintptr_t TORSO_2 = TORSO_1 + 0x20;
-    uintptr_t MID = TORSO_2 + 0x20;
-    uintptr_t LEFTARM = MID + 0x20;
-    uintptr_t RIGHTARM = LEFTARM + 0x20;
-    uintptr_t LEFTLEG_1 = RIGHTARM + 0x20;
-    uintptr_t RIGHTLEG_1 = LEFTLEG_1 + 0x20;
-    uintptr_t LEFTLEG_2 = RIGHTLEG_1 + 0x20;
-    uintptr_t RIGHTLEG_2 = LEFTLEG_2 + 0x20;
-#else
-    uintptr_t HEAD = 0xD31C88;
-    uintptr_t TORSO_1 = HEAD + 0x18;
-    uintptr_t TORSO_2 = TORSO_1 + 0x18;
-    uintptr_t MID = TORSO_2 + 0x18;
-    uintptr_t LEFTARM = MID + 0x18;
-    uintptr_t RIGHTARM = LEFTARM + 0x18;
-    uintptr_t LEFTLEG_1 = RIGHTARM + 0x18;
-    uintptr_t RIGHTLEG_1 = LEFTLEG_1 + 0x18;
-    uintptr_t LEFTLEG_2 = RIGHTLEG_1 + 0x18;
-    uintptr_t RIGHTLEG_2 = LEFTLEG_2 + 0x18;
-#endif
+// HIT BOXES для aarch64
+uintptr_t HEAD = 0x14247F8 ;
+uintptr_t TORSO_1 = HEAD + 0x20;
+uintptr_t TORSO_2 = TORSO_1 + 0x20;
+uintptr_t MID = TORSO_2 + 0x20;
+uintptr_t LEFTARM = MID + 0x20;
+uintptr_t RIGHTARM = LEFTARM + 0x20;
+uintptr_t LEFTLEG_1 = RIGHTARM + 0x20;
+uintptr_t RIGHTLEG_1 = LEFTLEG_1 + 0x20;
+uintptr_t LEFTLEG_2 = RIGHTLEG_1 + 0x20;
+uintptr_t RIGHTLEG_2 = LEFTLEG_2 + 0x20;
 
 #define libName "libblackrussia-client.so"
 
 float currentMultiplier = 4.0f;
 
-// Функция применения хитбоксов
 void ApplyHitboxes(float mult) {
     currentMultiplier = mult;
     Utils::WriteMemory<float>(getAbsoluteAddress(libName, HEAD), 0.15f * mult);
@@ -48,11 +34,9 @@ void ApplyHitboxes(float mult) {
     Utils::WriteMemory<float>(getAbsoluteAddress(libName, RIGHTLEG_2), 0.15f * mult);
 }
 
-// JNI sendChatMessage
 typedef void (*SendChatFn)(JNIEnv* env, jobject thiz, jstring msg);
 SendChatFn sendChat = nullptr;
 
-// Отправка сообщения в чат
 void SendChatMessage(JNIEnv* env, jobject thiz, const char* text) {
     if (sendChat) {
         jstring jmsg = env->NewStringUTF(text);
@@ -61,19 +45,6 @@ void SendChatMessage(JNIEnv* env, jobject thiz, const char* text) {
     }
 }
 
-void *main_thread(void *) {
-    do { sleep(1); } while (!isLibraryLoaded(libName));
-
-    uintptr_t libBase = (uintptr_t)getAbsoluteAddress(libName, 0);
-    sendChat = (SendChatFn)(libBase + 0x00ce4fe4);  // оффсет sendChatMessage
-
-    ApplyHitboxes(currentMultiplier);
-
-    pthread_exit(nullptr);
-    return nullptr;
-}
-
-// Обработчик команды (вызывай из хука)
 void ProcessChatCommand(JNIEnv* env, jobject thiz, const char* message) {
     std::string msg(message);
     if (msg.rfind("/sethb ", 0) == 0) {
@@ -87,6 +58,18 @@ void ProcessChatCommand(JNIEnv* env, jobject thiz, const char* message) {
             }
         } catch (...) {}
     }
+}
+
+void *main_thread(void *) {
+    do { sleep(1); } while (!isLibraryLoaded(libName));
+
+    uintptr_t libBase = (uintptr_t)getAbsoluteAddress(libName, 0);
+    sendChat = (SendChatFn)(libBase + 0x00ce4fe4);
+
+    ApplyHitboxes(currentMultiplier);
+
+    pthread_exit(nullptr);
+    return nullptr;
 }
 
 __attribute__((constructor)) void _init(){
